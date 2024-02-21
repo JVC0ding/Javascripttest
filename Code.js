82a97cb87413ac2360b53be6fa82fb5300c7a3a1
JVc0ding <J.velazquez.coding@gmail.com>
2024-02-21 15:42:54 -0500

    Initial coding

---
+++ Code.js
@@ -0,0 +1,39 @@
+// var a
+// let a
+// const a
+
+let a = 2
+let b = 1;
+
+ // b = b + 1
+ b++
+ 
+
+
+ if( a < b ) {
+    console.log ("this expression evaluates to true")
+ } else{
+console.log("the expression evaluated to false")
+console.log("") 
+
+ }
+
+
+
+
+
+
+
+// looping: while or for
+b = 0;
+while( b < 5){
+    console.log ("inside of while loop")
+    b++
+}
+
+
+
+
+
+
+ console.log("end")
